94d8bf93603efffc9782429cab529330a7f2132e
mbur <MB@Michaels-MacBook-Pro-2.local>
2015-05-07 20:39:24 -0700

    MB changes

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -2,4 +2,25 @@ console.log("this is a sanity check");
 
 $(document).ready(function(){
   console.log("Let's get coding!")
+
+// var currentColor = $("input");
+// var newColor = currentColor.val();
+
+var currentColor = $("input").val();
+
+$("input").change(function(){
+	console.log("Color changed");
+	currentColor = $("input").val();
 })
+
+$("td").mouseover(function() {
+	$(this).css("background-color", newColor);
+})
+
+
+
+})
+
+// $("td").mouseover(function() {
+// 	console.log("here");
+// })
